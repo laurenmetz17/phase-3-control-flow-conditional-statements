202d73118a1868220e49cd89cf3dfc9b190c4714
Lauren Metz <Laurenmetz15@gmail.com>
2022-12-27 15:01:42 -0500

    commit tree 4ec68ca04a0bd4adb2b9d1c482357ef563ffb5ab

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,46 @@
 def admin_login(username, password)
-  # your code here
+  if username == "admin" || username == "ADMIN"
+    if password == "12345"
+      "Access granted"
+    else
+      "Access denied"
+    end
+  else 
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  response = case temperature
+             when 0..40 then "It's brisk out there!"
+             when 40..65 then "It's a little chilly out there!"
+             when 85..100000 then "It's too dang hot out there!"
+             else "It's perfect out there!"
+             end
+  response
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 
+    if num % 5 == 0 
+      "FizzBuzz"
+    else 
+      "Fizz"
+    end
+  elsif num % 5 == 0
+    "Buzz"
+  else 
+    num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+case operation 
+when "+" then num1 + num2
+when "-" then num1 - num2
+when "*" then num1 * num2
+when "/" then num1 / num2
+else puts "Invalid operation!"
+end
 end
 
